6301928605ff8a4badd6a79ce86a09f1e575afaa
Jim Huang <jserv@biilabs.io>
2020-02-10 22:11:05 +0000

    Improve test cases

--- scripts/test.sh
+++ scripts/test.sh
@@ -40,15 +40,30 @@ test_op '1980+1'
 # sub
 test_op '2019-1'
 
-# testing div
+# div
 test_op '42/6'
-
 test_op '1/3'
-
 test_op '1/3*6+2/4'
-
 test_op '(1/3)+(2/3)'
-
 test_op '(2145%31)+23'
+
+# binary
+test_op '(3%0)|0' # should be 0
+test_op '1+2<<3' # should be (1 + 2) << 3 = 24
+test_op '123&42' # should be 42
+test_op '123^42' # should be 81
+
+# parens
+test_op '(((3)))*(1+(2))' # should be 9
+
+# assign
+test_op 'x=5, x=(x!=0)' # should be 1
+test_op 'x=5, x = x+1' # should be 6
+
+# fancy variable name
+test_op 'six=6, seven=7, six*seven' # should be 42
+test_op '小熊=6, 維尼=7, 小熊*維尼' # should be 42
+test_op 'τ=1.618, 3*τ' # should be 3 * 1.618 = 4.854
+test_op '$(τ, 1.618), 3*τ()' # shold be 3 * 1.618 = 4.854
 
 sudo rmmod calc
