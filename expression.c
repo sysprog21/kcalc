b0e14895052748ebcc0f0c2b14de3d70fd21637d
Jim Huang <jserv@biilabs.io>
2020-03-25 13:53:46 +0000

    Don't indent expression init script for pretty print

--- expression.c
+++ expression.c
@@ -92,15 +92,13 @@ enum expr_type {
 static int prec[] = {0, 1, 1, 1, 2, 2, 2, 2, 3,  3,  4,  4, 5, 5,
                      5, 5, 5, 5, 6, 7, 8, 9, 10, 11, 12, 0, 0, 0};
 
+/* clang-format off */
 #define expr_init()         \
     {                       \
         (enum expr_type) 0, \
-        {                   \
-            {               \
-                0           \
-            }               \
-        }                   \
+        { { 0 } }           \
     }
+/* clang-format on */
 
 static int expr_is_unary(enum expr_type op)
 {
